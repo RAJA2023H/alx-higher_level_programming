fb895641b600855addfa2d8c8b5d68e399d45a08
RAJA2023H <hemmanyrajae@gmail.com>
2023-10-04 16:07:19 -0700

    firt commit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upstr = ""
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            char = chr(ord(char) - 32)
+            upstr += char
+        else: upstr += char
+    print(upstr)
+    print()
+
