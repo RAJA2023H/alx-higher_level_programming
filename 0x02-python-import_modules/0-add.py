110026af8503fe68e53dfdc2de4c583c752268ab
RAJA2023H <hemmanyrajae@gmail.com>
2023-10-05 16:43:51 -0700

    adding readme file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add
+    import add_0
     a = 1
     b = 2
-    r = add_0. add(a, b)
+    r = add_0.add(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, r))
