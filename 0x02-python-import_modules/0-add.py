59ae820eeca390a518069a8a14e6044fce40eb43
RAJA2023H <hemmanyrajae@gmail.com>
2023-10-05 16:20:05 -0700

    if it main update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
-r = add_0.add(a, b)
-print("{} + {} = {}".format(a, b, r))
+if __name__ == "__main__":
+    import add_0
+    a = 1
+    b = 2
+    r = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, r))
