3893f2b923d05959af7c14974a96e78836bc7472
RAJA2023H <hemmanyrajae@gmail.com>
2023-10-04 18:27:03 -0700

    first commit

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for n in range(1, 101):
+        if (n % 3) == 0 and (n % 5) == 0:
+            print("fizzbuzz", end=" ")
+        elif (n % 3) == 0:
+            print("fizz", end=" ")
+        elif (n % 5) == 0:
+            print("buzz", end=" ")
+        else: print(n, end=" ")
