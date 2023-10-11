d2e5d49360c24a25c77602c52993b6152af2f423
RAJA2023H <hemmanyrajae@gmail.com>
2023-10-11 15:39:43 -0700

    pycodestyle

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,7 +4,7 @@ def square_matrix_simple(matrix=[]):
     for row in matrix:
         new_row = []
         for i in row:
-           v = i ** 2
-           new_row.append(v)
+            v = i ** 2
+            new_row.append(v)
         new_matrix.append(new_row)
     return new_matrix
