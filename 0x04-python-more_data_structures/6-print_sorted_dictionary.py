f0a2b4057a07608bcf057290d8e9de6897d53a99
RAJA2023H <hemmanyrajae@gmail.com>
2023-10-12 21:46:49 +0000

    first commit with ubuntu 14

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     for key in sorted(a_dictionary):
         value = a_dictionary[key]
-        print(f"{key}: {value}")
+        print(f"{key}:{value}")
--- 0x04-python-more_data_structures/7-main.py
+++ 0x04-python-more_data_structures/7-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-update_dictionary = __import__('7-update_dictionary').update_dictionary
+nary = __import__('7-update_dictionary').update_dictionary
 print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
 a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
