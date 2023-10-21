addccbb52ce9980a056b5dc9b86c11ecb2b6e83c
rajae <hemmanyrajae@gmail.com>
2023-10-21 10:49:54 -0700

    up

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,6 +3,13 @@
 def delete_at(my_list=[], idx=0):
     if my_list is None:
         return my_list
+
     if 0 <= idx < len(my_list):
-        my_list.pop(idx)
+        new_list = []
+        for i in range(len(my_list)):
+            if i != idx:
+                new_list.append(my_list[i])
+        return new_list
+
     return my_list
+
