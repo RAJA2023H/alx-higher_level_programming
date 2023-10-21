8074a197ca4bf5ed988dd71bddfd769629497d65
rajae <hemmanyrajae@gmail.com>
2023-10-21 10:27:31 -0700

    first try

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, element in enumerate(row):
+            print("{:d}".format(element), end="")
+            if i < len(row) - 1:
+                print(" ", end="")
+        print()
