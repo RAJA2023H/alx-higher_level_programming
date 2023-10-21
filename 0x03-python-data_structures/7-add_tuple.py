1ad767c4cc5e723df05187613b4d6c9108f241b6
rajae <hemmanyrajae@gmail.com>
2023-10-21 10:33:42 -0700

    pycodestyle

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,8 +4,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     # Ensure the tuples have at least 2 elements
     a = tuple_a + (0, 0)
     b = tuple_b + (0, 0)
-    
     # Sum the first two elements from each tuple
     result = (a[0] + b[0], a[1] + b[1])
-    
     return result
