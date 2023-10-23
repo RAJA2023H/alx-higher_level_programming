5332f844d11644c53c09e1b9f5dfdc4c7e2fca4e
rajae <hemmanyrajae@gmail.com>
2023-10-23 16:52:12 -0700

    up

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    printed =0
+    printed = 0
     for item in range(x):
         try:
             print("{:d}".format(my_list[item]), end="")
