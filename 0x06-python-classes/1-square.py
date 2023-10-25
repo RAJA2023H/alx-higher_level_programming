f7aa093ac4af3cedf83b56cb5e6ffa068776cd7a
rajae <hemmanyrajae@gmail.com>
2023-10-25 16:49:51 -0700

    up

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+class Square that defines a square
+"""
+
+
+class Square:
+    """
+    Private instance attribute
+    """
+    def __init__(self, __size=None):
+        self.__size = __size
