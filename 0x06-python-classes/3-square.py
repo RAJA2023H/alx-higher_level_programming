cf1dd07d3224f724dd2a15b369b17b9770f1e577
rajae <hemmanyrajae@gmail.com>
2023-10-25 17:13:21 -0700

    up

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+class Square that defines a square
+"""
+
+
+class Square:
+    """
+    initializing a private attribute for instance
+    """
+    def __init__(self, size=0):
+        """
+        checking if __size is integer
+        checking if __size is less than zero
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+    def area(self):
+        """
+        returns the current square area
+        """
+        return self.__size*self.__size
