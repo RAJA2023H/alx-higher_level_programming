44ace78c3001f590c2511c14b1cc1569fc76361a
rajae <hemmanyrajae@gmail.com>
2023-11-04 14:23:31 -0700

    up

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -37,7 +37,7 @@ def matrix_divided(matrix, div):
         divrow = []
         for i in row:
             if not isinstance(i, (int, float)):
-                raise TypeError(msg_err)
+                raise TypeError(Each row of the matrix must have the same size)
             x = i / div
             divrow.append(round(x, 2))
         divlist.append(divrow)
