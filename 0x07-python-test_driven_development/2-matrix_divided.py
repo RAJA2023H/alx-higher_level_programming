7575358cf0be7ab7c335aa9dd05216efaa178f8d
rajae <hemmanyrajae@gmail.com>
2023-11-04 07:06:36 -0700

    up

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,37 +1,46 @@
 #!/usr/bin/python3
 """
-Module composed by a function that divides the numbers of a matrix 
+Module composed by a function that divides the numbers of a matrix
 
 """
+
+
 def matrix_divided(matrix, div):
     """
     function that divides all elements of a matrix.
-    
+
     Args:
         matrix: list of lists of integers or floats
         div: a number
-    
+
     Returns a new matrix
-    
-    
+
+
     """
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
     divlist = []
-    if len(matrix) < 2 :
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    if len(matrix) < 2:
+        raise TypeError("matrix must be a matrix (list of lists) "
+                        "of integers/floats")
     ref_len = len(matrix[0])
+
     for row in matrix:
         if len(row) != ref_len:
-            raise TypeError("Each row of the matrix must have the same size")
+            raise TypeError("Each row of the matrix must have the "
+                            "same size")
         divrow = []
         for i in row:
             if not isinstance(i, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix "
+                                "(list of lists) of integers/floats")
             x = i / div
             divrow.append(round(x, 2))
         divlist.append(divrow)
+
     return divlist
