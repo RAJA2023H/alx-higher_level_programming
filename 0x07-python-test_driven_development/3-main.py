8079fd8124c213663027b328e0affea8cb72e8d6
rajae <hemmanyrajae@gmail.com>
2023-11-05 05:51:34 -0800

    up

--- 0x07-python-test_driven_development/3-main.py
+++ 0x07-python-test_driven_development/3-main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 say_my_name = __import__('3-say_my_name').say_my_name
 say_my_name("Bob")
-say_my_name("Bob", "Lenon")
+say_my_name("kawtar", "hm")
 try:
-    say_my_name(12, "White")
+    say_my_name("kawtar" , 12)
 except Exception as e:
     print(e)
