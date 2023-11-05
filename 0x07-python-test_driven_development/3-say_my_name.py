5772c288101b04a7ad9d3720ffa74cc516faef48
rajae <hemmanyrajae@gmail.com>
2023-11-05 04:49:09 -0800

    up

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module that import a function prints :
+    My name is <first name> <last name>
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    function that prints My name is <first name> <last name>
+    Args:
+        first name _ last name
+
+
+    """
+    if first_name:
+        if not isinstance(first_name, str):
+            raise TypeError("first_name must be a string")
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f" My name is {first_name} {last_name}")
