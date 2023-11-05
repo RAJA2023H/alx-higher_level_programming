f4ff558628e3b6e92a450e516ced346376793794
rajae <hemmanyrajae@gmail.com>
2023-11-05 05:23:54 -0800

    up

--- 0x07-python-test_driven_development/3-main.py
+++ 0x07-python-test_driven_development/3-main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 say_my_name = __import__('3-say_my_name').say_my_name
 
+say_my_name("Bob", "Lenon")
 try:
-    say_my_name(“Bob” / “Lenon”)
+    say_my_name(12, "White")
 except Exception as e:
     print(e)
--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -13,9 +13,7 @@ def say_my_name(first_name, last_name=""):
 
 
     """
-    if first_name:
-        if not isinstance(first_name, str):
-            raise TypeError("first_name must be a string")
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
