9dbb2b9b105ddadc7b02af6fe690347745be6c86
rajae <hemmanyrajae@gmail.com>
2023-11-05 18:55:39 -0800

    up

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+module import the function that  prints a text with
+2 new lines after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+    function that prints a text with 2 new lines after
+    each of these characters: ., ? and :
+
+    Args: text
+
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    delimiter_set = {".", "?", ":"}
+    current = ""
+    for char in text:
+        if char in delimiter_set:
+            if current:
+                print(current.strip(), end="")
+                current = ""
+            print(char)
+        else:
+            current += char
+    if current:
+        print(current.strip())
