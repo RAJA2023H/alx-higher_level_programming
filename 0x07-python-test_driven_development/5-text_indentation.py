d64be1bd32c46f912f17c1d29a9efa6b3c7e0a4a
rajae <hemmanyrajae@gmail.com>
2023-11-06 06:05:41 -0800

    up

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -28,6 +28,10 @@ def text_indentation(text):
                 print(char)
                 if i < len(text) - 1:
                     print()
+            else:
+                print(char)
+                if i < len(text) - 1:
+                    print()
         else:
             current += char
     if current and text[-1] not in delimiter_set:
