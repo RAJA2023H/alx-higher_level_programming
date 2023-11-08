603ea37a278cd4d2601de1c4b4316da56b9086ca
rajae <hemmanyrajae@gmail.com>
2023-11-08 10:02:27 -0800

    up

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module checks for exact same object
+"""
+
+def is_kind_of_class(obj, a_class):
+    """
+    function that returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited from,
+    the specified class ; otherwise False.
+    """
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
