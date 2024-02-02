f9d6ad93cd2ad47085410f169c8e28e349cbdce4
rajae <hemmanyrajae@gmail.com>
2024-02-02 03:56:42 +0000

    fixing syntax error

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+This is a docstring for Rectangle
+"""
+
+
+class Rectangle:
+    """
+    class that defines a rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
+        else:
+            raise TypeError("height must be an integer")
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
+        else:
+            raise TypeError("width must be an integer")
+
+    def area(self):
+        if self.__width == 0 and self.__height == 0:
+            return 0
+        return self.__width * self.__height
+    def perimeter(self):
+        return (self.__width + self.__height) * 2
