8ab9115114ae1abb6e29cd0493b4184eca7318fe
rajae <hemmanyrajae@gmail.com>
2024-02-04 00:57:35 +0000

    up

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -64,5 +64,5 @@ class Rectangle:
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
-        print ("Bye rectangle...")
+        print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
