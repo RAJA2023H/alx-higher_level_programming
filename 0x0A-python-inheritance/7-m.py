c383093594938354355b9fcfd64c3781830623ec
rajae <hemmanyrajae@gmail.com>
2024-02-26 22:35:08 -0800

    up

--- 0x0A-python-inheritance/7-m.py
+++ 0x0A-python-inheritance/7-m.py
@@ -4,6 +4,6 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 bg = BaseGeometry()
 
 try:
-    bg.integer_validator(“age”, [3])
+    bg.integer_validator(“age”, (4,))
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
