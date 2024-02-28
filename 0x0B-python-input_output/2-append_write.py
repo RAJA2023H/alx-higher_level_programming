88426c75007cc52a223e4d3c643c1d634b14b666
rajae <hemmanyrajae@gmail.com>
2024-02-27 21:55:02 -0800

    pycode style fixing

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,6 +3,8 @@
 function that appends a string at the end of a text
 file (UTF8) and returns the number of characters added:
 """
+
+
 def append_write(filename="", text=""):
     """
     Appends a string and returns the number of characters added
