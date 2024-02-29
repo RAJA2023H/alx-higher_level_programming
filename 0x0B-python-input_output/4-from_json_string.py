45365851a4a27c2139b8f19340e95e53c8c2797c
rajae <hemmanyrajae@gmail.com>
2024-02-28 16:27:38 -0800

    checking

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+returns an object (Python data structure)
+represented by a JSON string
+"""
+def from_json_string(my_str):
+    """
+    Returns an object (Python data structure)
+    represented by a JSON string
+    Args:
+        - my_str: the represented objed
+    """
+
