bd941c2877626b43675b403fde1a83abc70f4a19
rajae <hemmanyrajae@gmail.com>
2024-02-28 17:12:59 -0800

    using json.loads to load a string

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -3,6 +3,8 @@
 returns an object (Python data structure)
 represented by a JSON string
 """
+
+
 def from_json_string(my_str):
     """
     Returns an object (Python data structure)
@@ -10,4 +12,5 @@ def from_json_string(my_str):
     Args:
         - my_str: the represented objed
     """
-
+    import json
+    return json.loads(my_str)
