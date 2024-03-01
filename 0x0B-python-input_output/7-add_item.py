2515cd28a9e9d0826c7e9e9d9635370d7f3f7833
rajae <hemmanyrajae@gmail.com>
2024-02-29 23:18:21 -0800

    pycodestyle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,7 +4,7 @@ adds all arguments to a Python list,
 and then save them to a file
 """
 import sys
-save_to_json_file = __import__ ('5-save_to_json_file').save_to_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 args = sys.argv[1:]
 try:
