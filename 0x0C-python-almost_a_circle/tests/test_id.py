7881818e5c90d78e18a0ec2257aa0be94615d783
rajae <hemmanyrajae@gmail.com>
2024-03-03 15:49:31 -0800

    adding more test cases

--- 0x0C-python-almost_a_circle/tests/test_id.py
+++ 0x0C-python-almost_a_circle/tests/test_id.py
@@ -2,6 +2,12 @@
 import unittest
 from models.base import Base
 class testBase(unittest.TestCase):
-    def test_id(self):
+    def test_IDNONE(self):
         b1 = Base()
         self.assertEqual(b1.id, 1)
+    def test_IDNOTNONE(self):
+        b2 = Base(12)
+        self.assertEqual(b2.id, 12)
+    def test_THIRD_ATTRIBUTE(self):
+        b3 = Base()
+        self.assertEqual(b3.id, 2)
