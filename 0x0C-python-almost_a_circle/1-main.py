90f17849cb59b7faaf4a392d6917c49bd357c460
rajae <hemmanyrajae@gmail.com>
2024-03-07 09:41:14 -0800

    up

--- 0x0C-python-almost_a_circle/1-main.py
+++ 0x0C-python-almost_a_circle/1-main.py
@@ -4,7 +4,7 @@ from models.rectangle import Rectangle
 
 if __name__ == "__main__":
 
-    r1 = Rectangle(10, 2)
+    r1 = Rectangle(12, 14)
     print(r1.id)
 
     r2 = Rectangle(2, 10)
@@ -12,3 +12,7 @@ if __name__ == "__main__":
 
     r3 = Rectangle(10, 2, 0, 0, 12)
     print(r3.id)
+print('____')
+print(r1._Rectangle__width)
+print('____')
+print(r1._Rectangle__height)
---
+++ 0x0C-python-almost_a_circle/main_0.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Check """
+from models.rectangle import Rectangle
+
+r = Rectangle(12, 14)
+if r is None:
+    print("Can't create Rectangle")
+    exit(1)
+
+if r._Rectangle__width != 12:
+    print("Wrong width: {}".format(r._Rectangle__width))
+    exit(1)
+
+if r._Rectangle__height != 14:
+    print("Wrong height: {}".format(r._Rectangle__height))
+    exit(1)
+
+if r._Rectangle__x != 0:
+    print("Wrong x: {}".format(r._Rectangle__x))
+    exit(1)
+
+if r._Rectangle__y != 0:
+    print("Wrong y: {}".format(r._Rectangle__y))
+    exit(1)
+
+if r.id != 1:
+    print("ID is not initialized at 1")
+    exit(1)
+
+print("OK", end="")
