05436201eee5bf44ccd0a390236c5241ae5d3df8
rajae <hemmanyrajae@gmail.com>
2024-03-08 14:25:15 -0800

    up

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -101,3 +101,17 @@ class Rectangle(Base):
         x = self.__x
         y = self.__y
         return f"[Rectangle] ({Id}) {x}/{y} - {self.__width}/{self.__height}"
+
+    def update(self, *args):
+        """  assigns an argument to each attribute """
+        num_args = len(args)
+        if num_args > 0:
+            self.id = args[0]
+        if num_args > 1:
+            self.__width = args[1]
+        if num_args > 2:
+            self.__height = args[2]
+        if num_args > 3:
+            self.__x = args[3]
+        if num_args > 4:
+            self.__y = args[4]
