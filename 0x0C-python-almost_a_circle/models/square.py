0e3451b4d8d7704a89123cf9fdcc9ed6b4600f62
rajae <hemmanyrajae@gmail.com>
2024-03-08 20:48:50 -0800

    up

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+Write the class Square that inherits from Rectangle
 """
 from models.base import Base
 from models.rectangle import Rectangle
@@ -20,4 +21,4 @@ class Square(Rectangle):
         """
         returns string infor about the square
         """
-        return f"[Square] {self.id} {self.x}/{self.y} - {self.size}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
