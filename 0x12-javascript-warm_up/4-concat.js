22fa13046dcdcd6786ef3d772ce53bec6b9143af
Rajae <hemmanyrajae@gmail.com>
2024-04-08 20:22:10 -0700

    up

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
 const process = require('process');
 const args = process.argv;
-if (args[2]) {
-  console.log(args[2], 'is' , args[3] ? args[3] : 'undefined');
-} else {
-	console.log('undefined is undefined');
-}
-
+console.log(args[2] + 'is' + args[3]);
