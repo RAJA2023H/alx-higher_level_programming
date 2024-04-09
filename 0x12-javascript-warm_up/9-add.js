17790d59c69fa1ce6f6be99cf56314a644fed1b3
Rajae <hemmanyrajae@gmail.com>
2024-04-09 01:52:00 -0700

    up

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const n1 = parseInt(process.argv[2]);
 const n2 = parseInt(process.argv[3]);
-function add(a, b) {
+function add (a, b) {
   const sum = a + b;
   console.log(sum);
-  }
-add(n1,n2);
+}
+add(n1, n2);
