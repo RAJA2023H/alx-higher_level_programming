3addfc0ac9a383b2b4e467e5a2f072ae772db9c4
Rajae <hemmanyrajae@gmail.com>
2024-04-09 01:24:29 -0700

    upercase x

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,7 +6,7 @@ if (Number.isNaN(size)) {
   for (let i = 0; i < size; i++) {
     let array = [];
     for (let j = 0; j < size; j++) {
-      array += 'x';
+      array += 'X';
     }
     console.log(array);
   }
