39073f1e1f10796188379cbdb71c14614486434b
Rajae <hemmanyrajae@gmail.com>
2024-04-08 19:25:46 -0700

    up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 const process = require('process');
-if (process.argv.length < 3) {
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
+}
+if (process.argv.length === 3) {
   console.log('Arguments found');
-} else {
+}
+if (process.argv.length > 3){
   console.log('Arguments found');
 }
