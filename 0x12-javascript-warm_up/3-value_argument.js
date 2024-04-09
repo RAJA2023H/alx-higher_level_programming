75d838da70a341d36371a5998b28cc664f658823
Rajae <hemmanyrajae@gmail.com>
2024-04-08 19:44:40 -0700

    up

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const process = require('process');
 const args = process.argv;
-if (process.argv[2]) {
-  console.log(process.argv[2]);
+if (args[2]) {
+  console.log(args[2]);
 } else {
   console.log('No argument');
 }
