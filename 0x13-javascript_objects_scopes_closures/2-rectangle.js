d172aa556b4ce0a20bf98fc5bec258ed2e21883e
Rajae <hemmanyrajae@gmail.com>
2024-04-17 11:29:59 -0700

    up

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,9 +3,12 @@ class Rectangle {
   width;
   height;
 
-  constructor (w, h) {
+  constructor(w, h) {
     this.width = w;
     this.height = h;
   }
+}
+if (Rectangle.width <= 0 || Rectangle.height <= 0) {
+  
 }
 module.exports = Rectangle;
