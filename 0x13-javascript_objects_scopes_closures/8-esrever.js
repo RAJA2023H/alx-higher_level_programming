b0b0a72d3f6b8d9f5d98387a709da4b00391099e
Rajae <hemmanyrajae@gmail.com>
2024-04-18 14:49:19 -0700

    includ i = 0 in the interval

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const reversed = [];
-  for (let i = list.length - 1; i > 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     reversed.push(list[i]);
   }
   return reversed;
