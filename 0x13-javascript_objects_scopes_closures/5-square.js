5a1d0cf9352fed0f47a4b6c13401e856e145ebf0
Rajae <hemmanyrajae@gmail.com>
2024-04-17 22:48:14 -0700

    the super() call, the condition, are updated

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,8 +2,8 @@
 const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
+    super(Rectangle.width, Rectangle.height);
     if (size > 0) {
-      super(Rectangle.width, Rectangle.height);
       this.width = this.height = size;
     }
   }
