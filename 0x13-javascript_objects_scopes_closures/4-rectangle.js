34efe3bd33396f39031b013f18b0677e8839a187
Rajae <hemmanyrajae@gmail.com>
2024-04-17 22:12:39 -0700

    it's all about C logic

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+class Rectangle {
+  width;
+  height;
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  rotate() {
+  const temp = this.height;
+  this.height = this.width;
+  this.width = temp;
+  }
+
+  double() {
+  this.height *= 2;
+  this.width *= 2;
+  }
+}
+module.exports = Rectangle;
