693bd84e752e32140737fcda30616e7c508f4d81
Rajae <hemmanyrajae@gmail.com>
2024-04-18 14:26:59 -0700

    spelling errors

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let N = 0;
-  for (let i = 0; i < list.lenght; i++) {
+  for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
       N += 1;
     }
