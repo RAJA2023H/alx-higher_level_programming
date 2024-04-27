c341d31479ec10b4ce3896a404971d0879eb7b9d
Rajae <hemmanyrajae@gmail.com>
2024-04-26 19:22:12 -0700

    up

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter"""
+"""sends a POST request to the passed URL with the email as a parameter"""
 import sys
 import urllib.parse
 import urllib.request
