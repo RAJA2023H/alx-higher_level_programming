871c0af10266505b207dceb3897843b4c8a1a975
Rajae <hemmanyrajae@gmail.com>
2024-04-26 18:54:49 -0700

    up

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,9 +2,9 @@
 """sends a request to the URL"""
 import urllib.request
 import sys
-url = sys.argv[1]
-
-r = urllib.request.Request(url)
-with urllib.request.urlopen(r) as response:
-    h = response.headers['X-Request-Id']
-    print(h)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = urllib.request.Request(url)
+    with urllib.request.urlopen(r) as response:
+        h = response.headers['X-Request-Id']
+        print(h)
