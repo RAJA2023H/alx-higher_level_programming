6aa9959857b480719ba703741543b811dac3000f
Rajae <hemmanyrajae@gmail.com>
2024-04-26 18:06:25 -0700

    up

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+"""Python script fetch an url"""
 import urllib.request
-# Open and read the content of a URL
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+url = 'https://alx-intranet.hbtn.io/status'
+with urllib.request.urlopen(url) as response:
     response = response.read()
+    response_utf8 = response.decode('utf-8')
     print("Body response:")
     print(f" - type: {type(response)}")
     print(f" - content: {response}")
-    response_utf8 = response.decode('utf-8')
     print(f" - utf8 content: {response_utf8}")
