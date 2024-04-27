f13d16c6c6f6e4fd4657728bd699537149cba7e0
Rajae <hemmanyrajae@gmail.com>
2024-04-26 19:55:25 -0700

    up

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,4 +7,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     params = {"email": email}
-    print(requests.post(url, data=params).text)
+    r = requests.post(url, data=params)
+    print(res.text)
