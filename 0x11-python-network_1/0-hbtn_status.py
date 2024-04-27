4340caf2233ca58f2d1b2e46935549db6a3414ea
Rajae <hemmanyrajae@gmail.com>
2024-04-26 17:46:47 -0700

    first commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import urllib.request
+# Open and read the content of a URL
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    response = response.read()
+    print("Body response:")
+    print(f"  - type: {type(response)}")
+    print(f"  - content: {response}")
+    response_utf8 = response.decode('utf-8')
+    print(f"  - utf8 content: {response_utf8}")
