02bf4694449ca274faeee27cd50318d5c9f44816
Rajae <hemmanyrajae@gmail.com>
2024-04-26 19:40:02 -0700

    up

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,6 +8,5 @@ if __name__ == "__main__":
     r = requests.get(url)
     data = r.text
     rType = type(data)
-    print("Body response:")
-    print("\t- type: {}".format(type(rType)))
-    print("\t- content: {}".format(rType))
+    print(f"Body response:\n\t- type: {rType}\n\t\
+- content: {data}")
