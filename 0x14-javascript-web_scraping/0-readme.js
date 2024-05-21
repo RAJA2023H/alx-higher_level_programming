3813c9a73a0a0c9a240a12dd7cb4d9aab2650b7f
rajae <hemmanyrajae@gmail.com>
2024-05-21 16:27:53 -0700

    up

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// script that reads and prints the content of a file.
+const args = process.argv.slice(2);
+const fs = require('fs');
+const filepath = args[0];
+
+if (args.length < 1) {
+	console.error('Usage: node script.js <file_path>');
+	process.exit(1);
+}
+
+fs.readFile(filepath, 'utf8', (err, content) => {
+	if (err)
+	{
+		console.error('Error:', err);
+		return;
+	}
+	console.log(content);
+});
