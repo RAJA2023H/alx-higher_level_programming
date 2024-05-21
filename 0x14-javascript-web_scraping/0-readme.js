5e3715c61e5d6b321dfd11fdf73f4d1fba295cf0
rajae <hemmanyrajae@gmail.com>
2024-05-21 16:33:55 -0700

    up

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,15 +5,14 @@ const fs = require('fs');
 const filepath = args[0];
 
 if (args.length < 1) {
-	console.error('Usage: node script.js <file_path>');
-	process.exit(1);
+  console.error('Usage: node script.js <file_path>');
+  process.exit(1);
 }
 
 fs.readFile(filepath, 'utf8', (err, content) => {
-	if (err)
-	{
-		console.error('Error:', err);
-		return;
-	}
-	console.log(content);
+  if (err) {
+    console.error('Error:', err);
+    return;
+  }
+  console.log(content);
 });
