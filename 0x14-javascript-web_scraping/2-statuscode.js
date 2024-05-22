ab744ca3dedd88814b3ed6dcc4096958b3ccca6c
rajae <hemmanyrajae@gmail.com>
2024-05-21 18:03:29 -0700

    up

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,15 +1,17 @@
 #!/usr/bin/node
 // display the status code of a GET request.
 
-const https = require('https');
+const request = require('request');
 const url = process.argv[2];
 if (process.argv.length < 3) {
   console.error('Usage: node script.js <url>');
   process.exit(1);
 }
 
-https.get(url, (res) => {
-  console.log('Code:', res.statusCode);
-}).on('error', (err) => {
-  console.error('Error:', err.message);
+request(url, (error, response) => {
+  if (error) {
+    console.error('Error:', error.message);
+  } else {
+    console.log('Code:', response.statusCode);
+  }
 });
